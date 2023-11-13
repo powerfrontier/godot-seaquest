c69e3cb16860d07c9198b951fa0dc523e8e0dc12
frontier <power_frontier@yahoo.es>
2023-11-13 04:36:37 +0100

    new spawn system initiatied

--- enemies/enemy_spawner/enemy_spawner.gd
+++ enemies/enemy_spawner/enemy_spawner.gd
@@ -1,19 +1,24 @@
-extends Marker2D
+extends Node2D
 
-const Shark = preload("res://enemies/shark/shark.tscn")
 
-@onready var timer = $Timer
+@onready var left = $Left
+@onready var right = $Right
+const shark = preload("res://enemies/shark/shark.tscn")
 
-@export var reverse = false
-
-func _ready():
-	timer.wait_time = randf_range(0.5, 5)
-	timer.start()
 
 func _on_timer_timeout():
-	var shark_instance = Shark.instantiate()
+	var rnd_spawn_side = bool(randi_range(0,1))
+	var spawn_side = left
+	if rnd_spawn_side:
+		spawn_side = right
+	var spawn_marker = randi_range(1, 4)
+	
+	var shark_instance = shark.instantiate()
 	get_tree().current_scene.add_child(shark_instance)
-	shark_instance.global_position = global_position
-	timer.wait_time = randf_range(0.5, 5)
-	if reverse:
+	if rnd_spawn_side:
 		shark_instance.flip_direction()
+	var spawn_position = spawn_side.get_node(str(spawn_marker)).global_position
+	
+	shark_instance.global_position = spawn_position
+	
+	
