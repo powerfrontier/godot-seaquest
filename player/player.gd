41111175e172fdced49862210ebe9a20371038cd
frontier <frontier@avalon-mini.lan>
2023-11-27 21:45:09 +0100

    water background and clamp positons of the player

--- player/player.gd
+++ player/player.gd
@@ -5,8 +5,17 @@ var can_shoot = true
 var state = "default"
 
 const SPEED = Vector2(125, 90)
+
 const OXYGEN_DECREASE_SPEED = 2.5
 const OXYGEN_INCREASE_SPEED = 60
+const OXYGEN_MOVE_SHORE_LINE_SPEED = 50
+const OXYGEN_SHORE_LINE = 38
+
+const MAX_X_POSITION = 248
+const MIN_X_POSITION = 10
+const MAX_Y_POSITION = 205
+const MIN_Y_POSITION = OXYGEN_SHORE_LINE
+
 const BULLET_OFFSET = 7
 const Bullet = preload("res://player/player_bullet/player_bullet.tscn")
 
@@ -22,12 +31,15 @@ func _process(delta):
 		lose_oxygen(delta)
 	elif state == "less people oxygen refuel":
 		gain_oxygen(delta)
+		move_to_shore_line(delta)
 	elif state == "full people oxygen refuel":
 		gain_oxygen(delta)
+		move_to_shore_line(delta)
 
 func _physics_process(delta):
 	if state == "default":
 		movement(delta)
+	clamp_postion()
 
 func process_movement_input():
 	velocity.x = Input.get_axis("move_left", "move_right")
@@ -67,6 +79,16 @@ func gain_oxygen(delta):
 
 func movement(delta):
 	global_position += velocity * SPEED * delta
+
+func clamp_postion():
+	global_position.x = clamp(global_position.x, MIN_X_POSITION, MAX_X_POSITION)
+	global_position.y = clamp(global_position.y, MIN_Y_POSITION, MAX_Y_POSITION)
+
+func move_to_shore_line(delta):
+	global_position.y = move_toward(global_position.y, OXYGEN_SHORE_LINE, OXYGEN_MOVE_SHORE_LINE_SPEED * get_process_delta_time())
+	# igual pero sin el mov_towards
+	#if (global_position.y > OXYGEN_SHORE_LINE):
+	#	global_position.y -= OXYGEN_MOVE_SHORE_LINE_SPEED * delta 
 
 func _on_reload_timer_timeout():
 	can_shoot = true
